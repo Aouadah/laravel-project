6a3a8545d24dd80ae98b11b6d9807f95226daee4
aouad <aouadah2004@gmail.com>
2023-09-22 20:04:23 +0200

    verder gewerkt aan database

--- database/migrations/2023_09_18_123702_create_products_table.php
+++ database/migrations/2023_09_18_123702_create_products_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('username');
+            $table->string('email')->unique();
             $table->timestamps();
         });
     }
